f3edb67d1b7d9da85da027f2a2daa70a46d31924
zc3hd <23397295+zc3hd@users.noreply.github.com>
2019-12-16 17:31:24 +0800

    upd:rename-->cli-gulp-epr

--- api_server/app_auto.js
+++ api_server/app_auto.js
@@ -23,7 +23,6 @@ nodemon({
     path.join(__dirname, '../src_webapp/'),
     path.join(__dirname, '../webapp/'),
     path.join(__dirname, '../gulpfile.js'),
-    path.join(__dirname, '../cmd.js'),
   ],
   env: { 'NODE_ENV': 'development' }
 });
